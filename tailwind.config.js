c892b53d926934b16e9a379f6e562b931d672e21
Santos Osmin Urrutia <urrutia.osmin@gmail.com>
2020-12-02 10:37:59 -0600

    installing tailwind form and extending colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF2C5E',
+        main: '#4CA1AF',
+      },
+      gradientColorStops: {
+        main: '#4CA1AF',
+        end: '#2C3E50',  
+      }
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
