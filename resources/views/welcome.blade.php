7873132880f9b0531c7baf539954c6ece940fed9
Santos Osmin Urrutia <urrutia.osmin@gmail.com>
2020-12-02 20:57:40 -0600

    using app layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,22 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <title>Laravel</title>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body class="antialiased">
-        <h1 class="text-green-700 text-4xl font-bold">Prueba del home</h1>
-    </body>
-</html>
+<x-app-layout>
+    <div class="my-10">
+        <h2 class="text-3xl text-primary font-bold">Listado de anuncios</h2>
+
+        <div class="mt-5 grid grap-6">
+            <div class="bg-gray-200 rounded shadow p-5">
+                <div class="flex space-x-10">
+                    <div class="w-52 ">
+                        <div class="">
+                            <img class="rounded-full h-28 w-28" src=https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=991&q=80"" alt="">
+                        </div>
+                        <p class="text-main text-lg font-semibold text-center mt-2">Marion lee</p>
+                    </div>
+                    <div class="border-l-4 border-gray-300"></div>
+                    <div class="font-semibold text-lg">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio odit necessitatibus placeat nisi quasi et blanditiis at eligendi voluptate, nostrum neque perferendis doloremque alias officiis ipsa exercitationem delectus iusto illum.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
